39fc440ede6506b349314bfbd7fe780d068c62cc
meloegel <meloegel@gmail.com>
2022-02-08 23:03:43 -0500

    initial commit

---
+++ src/main/kotlin/com/mloegel/demo/SuploadApplication.kt
@@ -0,0 +1,11 @@
+package com.mloegel.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SuploadApplication
+
+fun main(args: Array<String>) {
+	runApplication<SuploadApplication>(*args)
+}
---
+++ src/test/kotlin/com/mloegel/demo/SuploadApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mloegel.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SuploadApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
