05195b3657c7ed5cf2e2df9c644c31195728a416
meloegel <meloegel@gmail.com>
2022-02-16 22:35:26 -0500

    laid groundwork for PdfGenerator

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,10 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("org.apache.pdfbox:pdfbox:2.0.24")
+	implementation("org.apache.pdfbox:xmpbox:2.0.24")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
---
+++ src/main/kotlin/com/mloegel/supload/contact/PdfGenerator.kt
@@ -0,0 +1,13 @@
+package com.mloegel.supload.contact
+
+import java.io.ByteArrayOutputStream
+
+
+class PdfGenerator {
+
+    fun generate(): ByteArrayOutputStream {
+
+        val baos = ByteArrayOutputStream()
+        return baos
+    }
+}
