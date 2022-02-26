0f87a8e93e12ea4786918575b2b42941fc5e7cbe
meloegel <meloegel@gmail.com>
2022-02-26 15:44:23 -0500

    added getAllContacts test

--- src/test/kotlin/com/mloegel/supload/contact/ContactControllerTest.kt
+++ src/test/kotlin/com/mloegel/supload/contact/ContactControllerTest.kt
@@ -1,16 +1,29 @@
 package com.mloegel.supload.contact
 
+import com.nhaarman.mockitokotlin2.whenever
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.reactive.server.WebTestClient
 
 
 @WebFluxTest(ContactController::class)
 internal class ContactControllerTest(@Autowired private val webClient: WebTestClient) {
 
+    @MockBean
+    private lateinit var mockContactService: ContactService
+
+    private val contactList = mutableListOf<Contact>()
+
     @Test
     fun getAllContacts() {
+        whenever(mockContactService.findAllContacts())
+            .thenReturn(contactList)
+        webClient.get()
+            .uri("/contacts")
+            .exchange()
+            .expectStatus().isOk
     }
 
     @Test
