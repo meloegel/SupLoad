50291f3fb9a5d5fa3b6228d1d7ace81654379508
meloegel <meloegel@gmail.com>
2022-03-12 23:24:48 -0500

    added 3 contacts into seed data

--- src/main/kotlin/com/mloegel/supload/SeedData.kt
+++ src/main/kotlin/com/mloegel/supload/SeedData.kt
@@ -1,6 +1,7 @@
 package com.mloegel.supload
 
 
+import com.mloegel.supload.contact.Contact
 import com.mloegel.supload.contact.ContactService
 import com.mloegel.supload.user.User
 import com.mloegel.supload.user.UserService
@@ -28,22 +29,32 @@ class SeedData : CommandLineRunner {
         userService?.deleteAll()
         contactService?.deleteAll()
 
-        val u1 = User(1,"admin", "password", "admin@lambdaschool.local")
+        val u1 = User(1, "admin", "password", "admin@lambdaschool.local")
         userService?.postUser(u1)
 
-        val u2 = User(6, "cinnamon", "1234567", "cinnamon@lambdaschool.local")
+        val u2 = User(2, "cinnamon", "1234567", "cinnamon@lambdaschool.local")
         userService?.postUser(u2)
 
-        val u3 = User(10, "barnbarn", "ILuvM4th!", "barnbarn@lambdaschool.local")
+        val u3 = User(3, "barnbarn", "ILuvM4th!", "barnbarn@lambdaschool.local")
         userService?.postUser(u3)
 
-        val u4 = User(15, "puttat", "password", "puttat@school.lambda")
+        val u4 = User(4, "puttat", "password", "puttat@school.lambda")
         userService?.postUser(u4)
 
-        val u5 = User( 17, "misskitty", "password", "misskitty@school.lambda")
+        val u5 = User(5, "misskitty", "password", "misskitty@school.lambda")
         userService?.postUser(u5)
 
-        val u6 = User(18,"fart", "password", "email@email.com")
+        val u6 = User(6, "fart", "password", "email@email.com")
         userService?.postUser(u6)
+
+        val c1 = Contact(1, "Bill", "Smith", "bill@email.com", "123 Main St", "Detroit", "MI", 12345, "555-555-5555")
+        contactService?.postContact(c1)
+
+        val c2 =
+            Contact(2, "Ted", "Potter", "ted@email.com", "345 Main St", "San Fransisco", "CA", 12345, "555-555-5555")
+        contactService?.postContact(c2)
+
+        val c3 = Contact(3, "Fred", "Johnson", "fred@email.com", "789 Main St", "Chicago", "IL", 12345, "555-555-5555")
+        contactService?.postContact(c3)
     }
 }
