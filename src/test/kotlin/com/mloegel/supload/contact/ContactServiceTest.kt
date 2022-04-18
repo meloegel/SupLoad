c85f3380bbfb6a10498a1aab15d7dbdcc19860e6
meloegel <meloegel@gmail.com>
2022-04-17 23:29:26 -0400

    added test for findContactByUsername

--- src/test/kotlin/com/mloegel/supload/contact/ContactServiceTest.kt
+++ src/test/kotlin/com/mloegel/supload/contact/ContactServiceTest.kt
@@ -1,6 +1,6 @@
 package com.mloegel.supload.contact
 
-import com.mloegel.supload.contact.pdf.ContactUploadParser
+import com.mloegel.supload.user.User
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import org.junit.jupiter.api.Test
@@ -11,7 +11,8 @@ internal class ContactServiceTest {
         Contact(1, "test", "testerson", "test@email.com", "123 main st", "test", "test", "555-555-5555", 55555)
 
     private val mockRepository = mock<ContactRepository>()
-    private val mockContactUploadParser = mock<ContactUploadParser>()
+
+    //    private val mockContactUploadParser = mock<ContactUploadParser>()
     private val contactService = ContactService(mockRepository)
 
     @Test
@@ -37,6 +38,13 @@ internal class ContactServiceTest {
         contactService.searchContactsByLastname("test")
         verify(mockRepository).findContactsByLastnameContainingIgnoreCase("test")
     }
+
+    @Test
+    fun findContactByUsername() {
+        contactService.findContactByUsername(User(50, "testUsername", "test", "test@email.com"))
+        verify(mockRepository).findContactByUser(User(50, "testUsername", "test", "test@email.com"))
+    }
+
 
     @Test
     fun postContact() {
